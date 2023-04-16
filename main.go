cca99c76f64734097c3b2562bcd3940a85693fe3
WoodProgrammer <asddeneme3@gmail.com>
2023-04-16 06:21:11 +0300

    idempotent file checker added

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
 )
+
+func checkHostsFile() {
+	if _, err := os.Stat("hosts.txt"); errors.Is(err, os.ErrNotExist) {
+		fmt.Println("hosts.txt file is not exist creating .... ")
+		fmt.Println("This file contains sample DNS records to track internal and external DNS resolution time period")
+
+		f, err := os.Create("hosts.txt")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		_, err2 := f.WriteString("www.youtube.com\nwww.google.com\n")
+
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	}
+
+}
 
 func main() {
 
 	fmt.Println("kubectl network doctor 0.0.1")
-
 	pluginScript := "plugin/main.sh"
+	checkHostsFile()
 
 	cmd := exec.Command(pluginScript)
 	stdout, err := cmd.StdoutPipe()
